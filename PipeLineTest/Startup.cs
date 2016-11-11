fc54643e7688c5d4ac45cd06ba99b0e1dec81520
jeferrie <jeferrie@MININT-2J4E4HS>
2016-11-10 16:49:19 -0800

    Add project files.

---
+++ PipeLineTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PipeLineTest.Startup))]
+namespace PipeLineTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
