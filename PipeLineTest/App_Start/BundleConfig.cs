30a5dfdba339776eadfc0d1e11d509b452e60fcc
jeferrie <ferriesj@gmail.com>
2016-11-11 10:22:14 -0800

    Added Cerulean bootstrap

--- PipeLineTest/App_Start/BundleConfig.cs
+++ PipeLineTest/App_Start/BundleConfig.cs
@@ -19,12 +19,12 @@ namespace PipeLineTest
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+            bundles.Add(new ScriptBundle("~/bundles/CeruleanBootstrap.css").Include(
+                      "~/Scripts/CeruleanBootstrap.css.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/CeruleanBootstrap.css",
                       "~/Content/site.css"));
         }
     }
